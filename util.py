9abc428133775d1ba0d3c23daba4c73c48f3c53f
박찬우 (AI 팀) <parkchanwoo@hodoo-09.hodoo.ai>
2019-10-01 16:53:57 +0000

    initial commit

---
+++ util.py
@@ -0,0 +1,380 @@
+import torch
+import torch.nn as nn
+import torchvision
+import sys
+
+import numpy as np
+from PIL import Image
+import PIL
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+import PIL.ImageDraw as ImageDraw
+import PIL.ImageFont as ImageFont
+
+def add_module(self, module):
+    self.add_module(str(len(self) + 1), module)
+    
+torch.nn.Module.add = add_module
+
+class Concat(nn.Module):
+    def __init__(self, dim, *args):
+        super(Concat, self).__init__()
+        self.dim = dim
+
+        for idx, module in enumerate(args):
+            self.add_module(str(idx), module)
+
+    def forward(self, input):
+        inputs = []
+        for module in self._modules.values():
+            inputs.append(module(input))
+
+        inputs_shapes2 = [x.shape[2] for x in inputs]
+        inputs_shapes3 = [x.shape[3] for x in inputs]        
+
+        if np.all(np.array(inputs_shapes2) == min(inputs_shapes2)) and np.all(np.array(inputs_shapes3) == min(inputs_shapes3)):
+            inputs_ = inputs
+        else:
+            target_shape2 = min(inputs_shapes2)
+            target_shape3 = min(inputs_shapes3)
+
+            inputs_ = []
+            for inp in inputs: 
+                diff2 = (inp.size(2) - target_shape2) // 2 
+                diff3 = (inp.size(3) - target_shape3) // 2 
+                inputs_.append(inp[:, :, diff2: diff2 + target_shape2, diff3:diff3 + target_shape3])
+
+        return torch.cat(inputs_, dim=self.dim)
+
+    def __len__(self):
+        return len(self._modules)
+
+
+class GenNoise(nn.Module):
+    def __init__(self, dim2):
+        super(GenNoise, self).__init__()
+        self.dim2 = dim2
+
+    def forward(self, input):
+        a = list(input.size())
+        a[1] = self.dim2
+        # print (input.data.type())
+
+        b = torch.zeros(a).type_as(input.data)
+        b.normal_()
+
+        x = torch.autograd.Variable(b)
+
+        return x
+
+
+class Swish(nn.Module):
+    """
+        https://arxiv.org/abs/1710.05941
+        The hype was so huge that I could not help but try it
+    """
+    def __init__(self):
+        super(Swish, self).__init__()
+        self.s = nn.Sigmoid()
+
+    def forward(self, x):
+        return x * self.s(x)
+
+
+def act(act_fun = 'LeakyReLU'):
+    '''
+        Either string defining an activation function or module (e.g. nn.ReLU)
+    '''
+    if isinstance(act_fun, str):
+        if act_fun == 'LeakyReLU':
+            return nn.LeakyReLU(0.2, inplace=True)
+        elif act_fun == 'Swish':
+            return Swish()
+        elif act_fun == 'ELU':
+            return nn.ELU()
+        elif act_fun == 'none':
+            return nn.Sequential()
+        else:
+            assert False
+    else:
+        return act_fun()
+
+
+def bn(num_features):
+    return nn.BatchNorm2d(num_features)
+
+
+def conv(in_f, out_f, kernel_size, stride=1, bias=True, pad='zero', downsample_mode='stride'):
+    downsampler = None
+    if stride != 1 and downsample_mode != 'stride':
+
+        if downsample_mode == 'avg':
+            downsampler = nn.AvgPool2d(stride, stride)
+        elif downsample_mode == 'max':
+            downsampler = nn.MaxPool2d(stride, stride)
+        elif downsample_mode  in ['lanczos2', 'lanczos3']:
+            downsampler = Downsampler(n_planes=out_f, factor=stride, kernel_type=downsample_mode, phase=0.5, preserve_size=True)
+        else:
+            assert False
+
+        stride = 1
+
+    padder = None
+    to_pad = int((kernel_size - 1) / 2)
+    if pad == 'reflection':
+        padder = nn.ReflectionPad2d(to_pad)
+        to_pad = 0
+  
+    convolver = nn.Conv2d(in_f, out_f, kernel_size, stride, padding=to_pad, bias=bias)
+
+
+    layers = filter(lambda x: x is not None, [padder, convolver, downsampler])
+    return nn.Sequential(*layers)
+
+
+
+
+def np_to_torch(img_np):
+    '''Converts image in numpy.array to torch.Tensor.
+    From C x W x H [0..1] to  C x W x H [0..1]
+    '''
+    return torch.from_numpy(img_np)[None, :]
+
+def get_text_mask(for_image, sz=20):
+    font_fname = '/usr/share/fonts/truetype/freefont/FreeSansBold.ttf'
+    font_size = sz
+    font = ImageFont.truetype(font_fname, font_size)
+
+    img_mask = Image.fromarray(np.array(for_image)*0+255)
+    draw = ImageDraw.Draw(img_mask)
+    draw.text((128, 128), "hello world", font=font, fill='rgb(0, 0, 0)')
+
+    return img_mask
+
+def get_bernoulli_mask(for_image, zero_fraction=0.95):
+    img_mask_np=(np.random.random_sample(size=pil_to_np(for_image).shape) > zero_fraction).astype(int)
+    img_mask = np_to_pil(img_mask_np)
+    
+    return img_mask
+
+def crop_image(img, d=32):
+    '''Make dimensions divisible by `d`'''
+
+    new_size = (img.size[0] - img.size[0] % d, 
+                img.size[1] - img.size[1] % d)
+
+    bbox = [
+            int((img.size[0] - new_size[0])/2), 
+            int((img.size[1] - new_size[1])/2),
+            int((img.size[0] + new_size[0])/2),
+            int((img.size[1] + new_size[1])/2),
+    ]
+
+    img_cropped = img.crop(bbox)
+    return img_cropped
+
+def np_to_pil(img_np): 
+    '''Converts image in np.array format to PIL image.
+    
+    From C x W x H [0..1] to  W x H x C [0...255]
+    '''
+    ar = np.clip(img_np*255,0,255).astype(np.uint8)
+    
+    if img_np.shape[0] == 1:
+        ar = ar[0]
+    else:
+        ar = ar.transpose(1, 2, 0)
+
+    return Image.fromarray(ar)
+
+def get_noisy_image(img_np, sigma):
+    """Adds Gaussian noise to an image.
+    
+    Args: 
+        img_np: image, np.array with values from 0 to 1
+        sigma: std of the noise
+    """
+    img_noisy_np = np.clip(img_np + np.random.normal(scale=sigma, size=img_np.shape), 0, 1).astype(np.float32)
+    img_noisy_pil = np_to_pil(img_noisy_np)
+
+    return img_noisy_pil, img_noisy_np
+
+def load(path):
+    """Load PIL image."""
+    img = Image.open(path)
+    return img
+
+def get_image(path, imsize=-1):
+    """Load an image and resize to a cpecific size. 
+    Args: 
+        path: path to image
+        imsize: tuple or scalar with dimensions; -1 for `no resize`
+    """
+    img = load(path)
+
+    if isinstance(imsize, int):
+        imsize = (imsize, imsize)
+
+    if imsize[0]!= -1 and img.size != imsize:
+        if imsize[0] > img.size[0]:
+            img = img.resize(imsize, Image.BICUBIC)
+        else:
+            img = img.resize(imsize, Image.ANTIALIAS)
+
+    img_np = pil_to_np(img)
+
+    return img, img_np
+
+def pil_to_np(img_PIL):
+    '''Converts image in PIL format to np.array.
+    
+    From W x H x C [0...255] to C x W x H [0..1]
+    '''
+    ar = np.array(img_PIL)
+
+    if len(ar.shape) == 3:
+        ar = ar.transpose(2,0,1)
+    else:
+        ar = ar[None, ...]
+
+    return ar.astype(np.float32) / 255.
+
+def plot_image_grid(images_np, nrow =8, factor=1, interpolation='lanczos'):
+    """Draws images in a grid
+    
+    Args:
+        images_np: list of images, each image is np.array of size 3xHxW of 1xHxW
+        nrow: how many images will be in one row
+        factor: size if the plt.figure 
+        interpolation: interpolation used in plt.imshow
+    """
+    n_channels = max(x.shape[0] for x in images_np)
+    assert (n_channels == 3) or (n_channels == 1), "images should have 1 or 3 channels"
+    
+    images_np = [x if (x.shape[0] == n_channels) else np.concatenate([x, x, x], axis=0) for x in images_np]
+
+    grid = get_image_grid(images_np, nrow)
+    
+    plt.figure(figsize=(len(images_np) + factor, 12 + factor))
+    
+    if images_np[0].shape[0] == 1:
+        plt.imshow(grid[0], cmap='gray', interpolation=interpolation)
+    else:
+        plt.imshow(grid.transpose(1, 2, 0), interpolation=interpolation)
+    
+    plt.show()
+    
+    return grid
+
+def get_image_grid(images_np, nrow=8):
+    '''Creates a grid from a list of images by concatenating them.'''
+    images_torch = [torch.from_numpy(x) for x in images_np]
+    torch_grid = torchvision.utils.make_grid(images_torch, nrow)
+    
+    return torch_grid.numpy()
+
+
+def fill_noise(x, noise_type):
+    """Fills tensor `x` with noise of type `noise_type`."""
+    if noise_type == 'u':
+        x.uniform_()
+    elif noise_type == 'n':
+        x.normal_() 
+    else:
+        assert False
+
+def get_noise(input_depth, method, spatial_size, noise_type='u', var=1./10):
+    """Returns a pytorch.Tensor of size (1 x `input_depth` x `spatial_size[0]` x `spatial_size[1]`) 
+    initialized in a specific way.
+    Args:
+        input_depth: number of channels in the tensor
+        method: `noise` for fillting tensor with noise; `meshgrid` for np.meshgrid
+        spatial_size: spatial size of the tensor to initialize
+        noise_type: 'u' for uniform; 'n' for normal
+        var: a factor, a noise will be multiplicated by. Basically it is standard deviation scaler. 
+    """
+    if isinstance(spatial_size, int):
+        spatial_size = (spatial_size, spatial_size)
+    if method == 'noise':
+        shape = [1, input_depth, spatial_size[0], spatial_size[1]]
+        net_input = torch.zeros(shape)
+        
+        fill_noise(net_input, noise_type)
+        net_input *= var            
+    elif method == 'meshgrid': 
+        assert input_depth == 2
+        X, Y = np.meshgrid(np.arange(0, spatial_size[1])/float(spatial_size[1]-1), np.arange(0, spatial_size[0])/float(spatial_size[0]-1))
+        meshgrid = np.concatenate([X[None,:], Y[None,:]])
+        net_input=  np_to_torch(meshgrid)
+    else:
+        assert False
+        
+    return net_input
+
+def get_params(opt_over, net, net_input, downsampler=None):
+    '''Returns parameters that we want to optimize over.
+    Args:
+        opt_over: comma separated list, e.g. "net,input" or "net"
+        net: network
+        net_input: torch.Tensor that stores input `z`
+    '''
+    opt_over_list = opt_over.split(',')
+    params = []
+    
+    for opt in opt_over_list:
+    
+        if opt == 'net':
+            params += [x for x in net.parameters() ]
+        elif  opt=='down':
+            assert downsampler is not None
+            params = [x for x in downsampler.parameters()]
+        elif opt == 'input':
+            net_input.requires_grad = True
+            params += [net_input]
+        else:
+            assert False, 'what is it?'
+            
+    return params
+
+def optimize(optimizer_type, parameters, closure, LR, num_iter):
+    """Runs optimization loop.
+    Args:
+        optimizer_type: 'LBFGS' of 'adam'
+        parameters: list of Tensors to optimize over
+        closure: function, that returns loss variable
+        LR: learning rate
+        num_iter: number of iterations 
+    """
+    if optimizer_type == 'LBFGS':
+        # Do several steps with adam first
+        optimizer = torch.optim.Adam(parameters, lr=0.001)
+        for j in range(100):
+            optimizer.zero_grad()
+            closure()
+            optimizer.step()
+
+        print('Starting optimization with LBFGS')        
+        def closure2():
+            optimizer.zero_grad()
+            return closure()
+        optimizer = torch.optim.LBFGS(parameters, max_iter=num_iter, lr=LR, tolerance_grad=-1, tolerance_change=-1)
+        optimizer.step(closure2)
+
+    elif optimizer_type == 'adam':
+        print('Starting optimization with ADAM')
+        optimizer = torch.optim.Adam(parameters, lr=LR)
+        
+        for j in range(num_iter):
+            optimizer.zero_grad()
+            closure()
+            optimizer.step()
+    else:
+        assert False
+
+def torch_to_np(img_var):
+    '''Converts an image in torch.Tensor format to np.array.
+    From 1 x C x W x H [0..1] to  C x W x H [0..1]
+    '''
+    return img_var.detach().cpu().numpy()[0]
